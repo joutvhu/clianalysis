be77b1d45e84167c9c56c66ea46e4c7151c65e23
Giao Ho <joutvhu@gmail.com>
2021-03-31 10:04:36 +0700

    Recommended

---
+++ src/recommend.ts
@@ -0,0 +1,20 @@
+import {CommandSchema} from './schema';
+
+export const helper: CommandSchema = {
+    exhale: (args) => {
+        for (let i = args.stack.length - 1; i > -1; i--) {
+            const stack = args.stack[i];
+            if (i === 0 || stack.type == null || stack.type === 'task') {
+                const help = stack.help;
+                if (typeof help === 'string')
+                    console.log(help);
+                else if (help instanceof Function || typeof help === 'function')
+                    help(args);
+                else continue;
+                break;
+            }
+        }
+
+        return true;
+    }
+};
